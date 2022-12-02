16559ff3165c575e2ffd308f9a12d0559f65c980
YuryZhehala <yury.zhehala@gmail.com>
2022-12-02 10:17:19 +0100

    feat: localization

--- routes/localization.js
+++ routes/localization.js
@@ -5,7 +5,7 @@ const { authenticateToken } = require("../middlewares/authenticateToken");
 const bcrypt = require("bcrypt");
 const multipart = require("connect-multiparty");
 
-const { getAll } = require("../db/localization.js");
+const { getAll, updateLocalisation } = require("../db/localization.js");
 const { signupValidation } = require("../helpers/validation.js");
 const { getRefreshToken, deleteRefreshToken } = require("../db/users");
 
@@ -20,5 +20,17 @@ router.get("/all", async (req, res) => {
       console.log("err", err);
     }
   });
+
+router.put("/", multipartMiddleware, async (req, res) => {
+  try {
+    if (!req.body) return res.sendStatus(400);
+    console.log("req.body", req.body)
+    const { locale, value, category, subcategory } = req.body
+
+    await updateLocalisation(locale, value, category, subcategory, res);
+  } catch (err) {
+    console.log("err", err);
+  }
+});
 
 module.exports = router;
